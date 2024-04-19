a5eaa38a917e69d7e3b774766e44b2f57d1220b2
revir216 <ducnm.nguyen@gmail.com>
2024-04-19 20:40:41 +0700

    Adding deployement and service

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0bde929851624259a11a87535c2629f-1269413698.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
